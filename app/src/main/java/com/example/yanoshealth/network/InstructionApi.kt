1e369c861a8c39461e36de2ea88ecd3d53d50953
RiyadHassen <riyadworku@gmail.com>
2020-04-21 00:27:07 +0300

    app configured to work on deveice

--- app/src/main/java/com/example/yanoshealth/network/InstructionApi.kt
+++ app/src/main/java/com/example/yanoshealth/network/InstructionApi.kt
@@ -11,7 +11,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL = "http://10.0.2.2:8080/"
+private const val BASE_URL = "http://192.168.43.22:8080/"
 
 /**
  * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
