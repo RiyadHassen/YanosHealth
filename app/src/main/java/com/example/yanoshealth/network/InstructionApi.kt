33d7c0d55d336dad6e2ec66ffb92de7446f70f71
RiyadHassen <riyadworku@gmail.com>
2020-06-05 18:24:37 +0300

    changed api

--- app/src/main/java/com/example/yanoshealth/network/InstructionApi.kt
+++ app/src/main/java/com/example/yanoshealth/network/InstructionApi.kt
@@ -11,7 +11,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL = "http://192.168.43.22:8080/"
+private const val BASE_URL = "http://10.0.2.2:5000/api/instructions"
 
 /**
  * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
